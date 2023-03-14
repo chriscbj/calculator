dc4f750a1dd354319cfc8f4a69d9012d438695d9
chriscbj <chriscbj@hotmail.com>
2023-03-14 23:21:30 +0800

    Added operate() function

--- js/script.js
+++ js/script.js
@@ -13,3 +13,16 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
+
+function operate(num1, num2, operator){
+    switch (operator){
+        case 1: return add(num1, num2);
+        break;
+        case 2: return subtract(num1, num2);
+        break;
+        case 3: return multiply(num1, num2);
+        break;
+        case 4: return divide(num1, num2);
+        break;
+    }
+};
