735d7deaf43fab40ba35afbd2e1e93eed83c0fb5
chriscbj <chriscbj@hotmail.com>
2023-03-19 01:47:21 +0800

    Added click event listener for number buttons and operators

--- js/script.js
+++ js/script.js
@@ -1,28 +1,79 @@
-function add(num1, num2){
-    return num1 + num2;
+const numberButtons = document.querySelector('.numberButtons');
+const display = document.querySelector('.display');
+const operators = document.querySelector('.operators');
+
+let firstNum = "";
+let secondNum = "";
+let nextNum = "";
+let operator = "";
+let finalSum = "";
+
+function reset(){
+    return secondNum = '';
+}
+
+function add(firstNum, secondNum){
+    return firstNum + secondNum;
 };
 
-function subtract(num1, num2){
-    return num1 - num2;
+function subtract(firstNum, secondNum){
+    return firstNum - secondNum;
 }
 
-function multiply(num1, num2){
-    return num1 * num2;
+function multiply(firstNum, secondNum){
+    return firstNum * secondNum;
 }
 
-function divide(num1, num2){
-    return num1 / num2;
+function divide(firstNum, secondNum){
+    return firstNum / secondNum;
 }
 
-function operate(num1, num2, operator){
+function operate(firstNum, secondNum, operator){
     switch (operator){
-        case 1: return add(num1, num2);
+        case "+": return add(parseInt(firstNum), parseInt(secondNum));
         break;
-        case 2: return subtract(num1, num2);
+        case "-": return subtract(parseInt(firstNum), parseInt(secondNum));
         break;
-        case 3: return multiply(num1, num2);
+        case "*": return multiply(parseInt(firstNum), parseInt(secondNum));
         break;
-        case 4: return divide(num1, num2);
+        case "/": return divide(parseInt(firstNum), parseInt(secondNum));
         break;
+    default:
+        return null;
     }
 };
+
+numberButtons.addEventListener('click', (e) => {
+    // console.log(e) to view e's attribute
+    if (e.target.matches('button')){ // checks if e.target is a button
+        if (operator === ""){ // if operator and firstNum is empty
+        firstNum += e.target.innerText; // set firstNum to button clicked
+        display.innerHTML = firstNum;
+        } else {
+            secondNum += e.target.innerText;
+            display.innerHTML = secondNum;
+        }
+        }
+    });
+/*     if (firstNum !== "" && secondNum !== "" && finalSum !== "" && operator !== ""){ // if finalSum exists
+        operator = "";
+        secondNum = "";
+        firstNum = finalSum;
+        secondNum = e.target.innerText; // set secondNum as button innerText
+        finalSum = operate(firstNum, secondNum, operator);
+    }
+        }
+    }
+}); */
+
+operators.addEventListener('click', (e) => {
+    if (e.target.innerText !== "="){
+        operator = e.target.innerText;
+    } else if (firstNum !== '' && secondNum !== '' && operator !== '') {
+        finalSum = operate(firstNum, secondNum, operator);
+        display.innerHTML = finalSum;
+    } else {
+        finalSum = operate(firstNum, nextNum, operator);
+        display.innerHTML = finalSum;
+    }
+});
